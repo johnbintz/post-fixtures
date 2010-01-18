81f6496efeee2d881ea36cdc1736e99e76e8bd94
John Bintz <john@coswellproductions.com>
2010-01-17 19:41:10 -0500

    working on builder

--- test/FixtureBuilderTest.php
+++ test/FixtureBuilderTest.php
@@ -20,7 +20,7 @@ class FixtureBuilderTest extends PHPUnit_Framework_TestCase {
 			),
 			array(
 				array(
-					array('category', array('test/test2'))
+					array('category', array('test,test2'))
 				),
 				array('category' => array('test', 'test2')),
 				array(true)
@@ -44,16 +44,22 @@ class FixtureBuilderTest extends PHPUnit_Framework_TestCase {
 			array(
 				array(
 					array('post', array('Post title')),
-					array('date', array('2010-01-01'))
+					array('date', array('2010-01-01')),
+					array('categories', array('test,test2')),
+					array('metadata', array('key', array('metadata' => 'value'))),
 				),
-				array('post' => array(
-					array(
-						'post_title' => 'Post title',
-						'post_date'  => '2010-01-01',
-						'post_type'  => 'post',
-					)
-				)),
-				array(true, true)
+				array(
+					'post' => array(
+						 array(
+							'post_title' => 'Post title',
+							'post_type'  => 'post',
+						 	'post_date'  => '2010-01-01',
+							'categories' => array('test', 'test2'),
+						 	'metadata' => array('key' => array('metadata' => 'value'))
+						)
+					),
+				),
+				array(true, true, true, true)
 			),
 		);
 	}
@@ -72,7 +78,7 @@ class FixtureBuilderTest extends PHPUnit_Framework_TestCase {
 				$return = call_user_func_array(array($builder, $instruction), $parameters);
 				$this->assertTrue($expected_exception);
 			} catch (Exception $e) {
-				$this->assertFalse($expected_exception);
+				$this->assertFalse($expected_exception, $e->getMessage());
 				$this->assertType('LogicException', $e);
 			}
 		}
@@ -80,5 +86,109 @@ class FixtureBuilderTest extends PHPUnit_Framework_TestCase {
 		$builder->defer();
 
 		$this->assertEquals($expected_deferred_build, $builder->deferred_builds);
+	}
+
+	function providerTestImmediateBuild() {
+		return array(
+			array(
+				array(
+					array(
+						'category',
+						array('test'),
+					),
+					array(
+						'category',
+						array('test/test2'),
+					)
+				),
+				array('test' => 1, 'test/test2' => 2),
+				array(
+					'categories' => array(
+						1 => 'test',
+						2 => 'test2'
+					)
+				)
+			),
+			array(
+				array(
+					array(
+						'post',
+						array('Post title')
+					),
+					array(
+						'date',
+						array('2010-01-01')
+					),
+					array(
+						'categories',
+						array('test')
+					),
+					array(
+						'metadata',
+						array('test', 'test2')
+					)
+				),
+				(object)array(
+					'post_title' => 'Post title',
+					'post_type' => 'post',
+					'post_date' => '2010-01-01',
+					'post_status' => 'publish',
+				'ID' => 1
+				),
+				array(
+					'posts' => array(
+						1 => (object)array(
+							'post_title' => 'Post title',
+							'post_type' => 'post',
+							'post_date' => '2010-01-01',
+							'post_status' => 'publish',
+							'ID' => 1
+						)
+					),
+					'categories' => array(
+						1 => 'test'
+					),
+					'post_meta' => array(
+						1 => array(
+							'test' => 'test2'
+						)
+					)
+				)
+			)
+		);
+	}
+
+	/**
+	 * @dataProvider providerTestImmediateBuild
+	 */
+	function testImmediateBuild($instructions, $expected_return, $wp_checks) {
+		$builder = new FixtureBuilder();
+		foreach ($instructions as $info) {
+			list($instruction, $parameters) = $info;
+			call_user_func_array(array($builder, $instruction), $parameters);
+		}
+		$this->assertEquals($expected_return, $builder->build());
+
+		foreach ($wp_checks as $type => $info) {
+			switch ($type) {
+				case 'posts':
+					foreach ($info as $key => $post) {
+						$this->assertEquals($post, get_post($key));
+					}
+					break;
+				case 'categories':
+					foreach ($info as $key => $category_name) {
+						$this->assertEquals($category_name, get_cat_name($key));
+					}
+					break;
+				case 'post_meta':
+					foreach ($info as $post_id => $meta_info) {
+						foreach ($meta_info as $key => $value) {
+							$this->assertEquals($value, get_post_meta($post_id, $key, true));
+						}
+					}
+					break;
+			}
+		}
 	}
 }
